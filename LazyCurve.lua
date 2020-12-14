18714c7884a0a386ee465b257a8734e1b9b2642b
Mark W <numedain@gmail.com>
2020-12-14 17:55:33 +0100

    should fix errors for people who newly install the addon, or reset the settings

--- LazyCurve.lua
+++ LazyCurve.lua
@@ -10,6 +10,9 @@ local LFGListApplicationDialog = _G.LFGListApplicationDialog
 local LE_EXPANSION_LEVEL_CURRENT = _G.LE_EXPANSION_LEVEL_CURRENT
 
 local name = ...
+
+LazyCurveDB = LazyCurveDB or {}
+
 local LazyCurve = LibStub('AceAddon-3.0'):NewAddon(name, 'AceConsole-3.0', 'AceHook-3.0', 'AceEvent-3.0');
 if not LazyCurve then return end
 
